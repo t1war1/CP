10d1beb5c2e195365a0791b154cb1c8be3507106
Gaurav <tiwarigaurav1998@gmail.com>
2019-04-14 21:05:19 +0530

    CF round551

---
+++ Codeforces/Round551/AServalAndBus.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define mod 1000000007ll
+#define mod2 100000009ll
+#define mod3 998244353
+#define pb push_back
+#define fastIO ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define readi(x) scanf("%d",&x)
+#define reads(x)  scanf("%s", x)
+#define readl(x) scanf("%I64d",&x)
+#define PI 3.141592653589793238462643383
+#define repi(i,a,b) for(int i=a;i<b;i++)
+#define repd(i,a,b) for(int i=a;i>b;i--)
+#define mp make_pair
+#define ll long long
+#define sorti(a,b) sort(a,b)
+#define sortd(a,b,tp) sort(a,b,greater<tp>())
+#define ff first
+#define ss second
+
+using namespace std;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<long double,long double>pdd;
+template<class T>
+using max_pq = priority_queue<T>;
+template<class T>
+using min_pq = priority_queue<T,vector<T>,greater<T>>;
+int oo = 0x3f3f3f3f;
+class AServalAndBus {
+    int n,t;
+    vector<pll> arr;
+public:
+	void solve(istream& cin, ostream& cout) {
+		cin>>n>>t;
+		arr.resize(n);
+		repi(i,0,n)
+        {
+		    cin>>arr[i].ff>>arr[i].ss;
+        }
+		ll ans=oo,ind;
+		for(int i=0;i<n;i++)
+        {
+		    if(arr[i].ff>=t)
+            {
+		        ans=min(ans,arr[i].ff);
+		        if(arr[i].ff==ans)
+		            ind=i+1;
+            } else{
+		        ll x=(ll)ceil(1.0*(t-arr[i].ff)/arr[i].ss);
+		        ans=min(ans,arr[i].ff+x*arr[i].ss);
+		        if((arr[i].ff+x*arr[i].ss)==ans)
+		            ind=i+1;
+		    }
+        }
+		cout<<ind;
+
+	}
+};
