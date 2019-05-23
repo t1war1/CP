e8fb772372a2949946bfcb502cca36a71e009c07
Gaurav <tiwarigaurav1998@gmail.com>
2019-05-24 01:25:21 +0530

    Library edit+upsolving

---
+++ Library/Sieve&Primes.cpp
@@ -0,0 +1,24 @@
+bool sieve[1000000];
+vector<int> primes;
+void sieveMake(int n)
+{
+	//change sieve size according to n
+	for(int i=0;i<n;i++)
+		sieve[i]=1;
+	sieve[0]=0;sieve[1]=0;
+	for(int i=2;i*i<n;i++)
+	{
+		if(sieve[i])
+		{
+			for(int j=i*i;j<n;j+=i)
+			{
+				sieve[j]=0;
+			}
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(sieve[i])
+			primes.pb(i);
+	}
+}
