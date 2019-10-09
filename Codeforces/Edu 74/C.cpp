c050ee59a85cefe31bfa6750861284bb821105be
Gaurav Tiwari <tiwarigaurav1998@gmail.com>
2019-10-09 23:18:27 +0530

    Cf edu 74

---
+++ Codeforces/Edu 74/C.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+#define pb      push_back
+#define fastIO  ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define PI      3.141592653589793238462643383
+#define mp      make_pair
+#define ff      first
+#define ss      second
+#define endl    "\n"
+#define all(v)  v.begin(),v.end()
+// #define int     long long
+ 
+using namespace std;
+typedef pair<int,int> pii;
+typedef pair<long double,long double>pdd;
+template<class T>
+using max_pq = priority_queue<T>;
+template<class T>
+using min_pq = priority_queue<T,vector<T>,greater<T> >;
+ 
+vector<string> split(const string& s, char c) {
+    vector<string> v; stringstream ss(s); string x;
+    while (getline(ss, x, c)) v.emplace_back(x); return move(v);
+}
+template<typename T, typename... Args>
+inline string arrStr(T arr, int n) {
+    stringstream s; s << "[";
+    for(int i = 0; i < n - 1; i++) s << arr[i] << ",";
+        if(n > 0)
+            s << arr[n - 1] ;
+        s<< "]";
+        return s.str();
+    }
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cerr << name << " : " << arg1 << endl;
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ','); cerr.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+    }
+#else
+#define trace(...)
+#endif
+ 
+ 
+int32_t main()
+{
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("debug.txt", "w", stderr);
+    // #endif
+ 
+    fastIO;
+ 
+    int testcases;cin>>testcases;
+    for(int test=1;test<=testcases;test++)
+    {
+        int n,h;
+        cin>>h>>n;
+        vector<int> arr;
+        for(int i=0;i<n;i++)
+        {
+            int x;
+            cin>>x;
+            arr.pb(x);
+        }
+        if(n==1 || h==1 || h==2)
+        {
+            cout<<0<<endl;
+            continue;
+        }
+        arr.pb(0);
+        int curr=h,i=1,ans=0;
+        while(i<n)
+        {
+            if((arr[i]+1-arr[i+1])>2)
+            {
+                ans++;
+                curr=arr[i]-1;
+                i++;
+            }
+            else
+            {
+                curr=arr[i+1];
+                i+=2;
+            }
+        }
+        cout<<ans<<endl;
+    }
+    
+    return 0;
+}
