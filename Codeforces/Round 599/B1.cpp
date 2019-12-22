d0348f51085e3e1022ecb2d4ef6e5a3dfe093cb3
Gaurav Tiwari <tiwarigaurav1998@gmail.com>
2019-12-23 02:09:14 +0530

    Round 599

---
+++ Codeforces/Round 599/B1.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+#define pb      push_back
+#define fastIO  ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define PI      3.141592653589793238462643383
+#define mp      make_pair
+#define ff      first
+#define ss      second
+#define endl    "\n"
+#define all(v)  v.begin(),v.end()
+#define int     long long
+ 
+using namespace std;
+typedef pair<int,int> pii;
+typedef pair<long double,long double>pdd;
+template<class T>
+using max_pq = priority_queue<T>;
+template<class T>
+using min_pq = priority_queue<T,vector<T>,greater<T> >;
+ 
+vector<string> split(const string& s, char c) {
+    vector<string> v; stringstream ss(s); string x;
+    while (getline(ss, x, c)) v.emplace_back(x); return move(v);
+}
+template<typename T, typename... Args>
+inline string arrStr(T arr, int n) {
+    stringstream s; s << "[";
+    for(int i = 0; i < n - 1; i++) s << arr[i] << ",";
+        if(n > 0)
+            s << arr[n - 1] ;
+        s<< "]";
+        return s.str();
+    }
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cerr << name << " : " << arg1 << endl;
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ','); cerr.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+    }
+#else
+#define trace(...)
+#endif
+//const int N=;
+ 
+ 
+int32_t main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("debug.txt", "w", stderr);
+    #endif
+ 
+    fastIO;
+ 
+    int testcases;cin>>testcases;
+    for(int test=1;test<=testcases;test++)
+    {
+        int n;
+        cin>>n;
+        string str1,str2;
+        cin>>str1>>str2;
+        int count=0, x=-1,y=-1;
+ 
+        for(int i=0;i<n;i++)
+        {
+            if(str1[i]!=str2[i])
+            {
+                count++;
+                if(x==-1)
+                {
+                    x=i;
+                }
+                else
+                {
+                    y=i;
+                }
+            }
+        }
+        if(count!=2)
+        {
+            cout<<"No";
+        }
+        else
+        {
+            if(str1[x]==str1[y] && str2[x]==str2[y])
+            {
+                cout<<"Yes";
+            }
+            else
+            {
+                cout<<"No";
+            }
+ 
+        }
+ 
+        cout<<endl;
+    }
+    
+    return 0;
+}
---
+++ Codeforces/Round 599/B2.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+#define pb      push_back
+#define fastIO  ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define PI      3.141592653589793238462643383
+#define mp      make_pair
+#define ff      first
+#define ss      second
+#define endl    "\n"
+#define all(v)  v.begin(),v.end()
+#define int     long long
+ 
+using namespace std;
+typedef pair<int,int> pii;
+typedef pair<long double,long double>pdd;
+template<class T>
+using max_pq = priority_queue<T>;
+template<class T>
+using min_pq = priority_queue<T,vector<T>,greater<T> >;
+ 
+vector<string> split(const string& s, char c) {
+    vector<string> v; stringstream ss(s); string x;
+    while (getline(ss, x, c)) v.emplace_back(x); return move(v);
+}
+template<typename T, typename... Args>
+inline string arrStr(T arr, int n) {
+    stringstream s; s << "[";
+    for(int i = 0; i < n - 1; i++) s << arr[i] << ",";
+        if(n > 0)
+            s << arr[n - 1] ;
+        s<< "]";
+        return s.str();
+    }
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cerr << name << " : " << arg1 << endl;
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ','); cerr.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+    }
+#else
+#define trace(...)
+#endif
+//const int N=;
+ 
+ 
+int32_t main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("debug.txt", "w", stderr);
+    #endif
+ 
+    fastIO;
+ 
+    int testcases;cin>>testcases;
+    for(int test=1;test<=testcases;test++)
+    {
+        int n;
+        cin>>n;
+        string s,t;
+        cin>>s>>t;
+ 
+        int arr[26];
+        for(int i=0;i<26;i++) arr[i]=0;
+ 
+        for(int i=0;i<n;i++)
+        {
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']++;
+        }
+        int flag=0;
+        for(int i=0;i<26;i++)
+        {
+            if(arr[i]&1)
+            {
+                flag=1;
+                break;
+            }
+        }
+ 
+        if(flag){
+            cout<<"No\n";
+        }
+        else
+        {
+            cout<<"Yes"<<endl;
+            int count=0;
+            vector<pii> ans;
+            for(int i=0;i<n;i++)
+            {
+                if(s[i]!=t[i])
+                {
+                    int f=0;
+                    for(int j=i+1;j<n;j++)
+                    {
+                        if(s[j]==s[i])
+                        {
+                            count++;
+                            ans.pb({j,i});
+                            swap(s[j],t[i]);
+                            f=1;
+                            break;
+                        }
+                    }
+                    if(!f)
+                    {
+                        for(int j=i+1;j<n;j++)
+                        {
+                            if(s[i]==t[j])
+                            {
+                                count+=2;
+                                ans.pb({j,j});
+                                swap(t[j],s[j]);
+                                swap(s[j],t[i]);
+                                ans.pb({j,i});
+                                f=1;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            cout<<count<<endl;
+            for(int i=0;i<ans.size();i++)
+            {
+                cout<<ans[i].ff+1<<" "<<ans[i].ss+1<<endl;
+            }
+        }
+    }
+    
+    return 0;
+}
