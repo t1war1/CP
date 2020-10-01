4c6a2f37b3f7d4eb49b7376337d57c4a3c88440d
Harsh Patel <harsh.patel.magic105@gmail.com>
2020-10-02 02:07:02 +0530

    template added

---
+++ Library/Template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define int int64_t
+#define M 1000000007
+#define pb push_back
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+
+
+
+int32_t main()
+{
+	//fastio;
+	int t=1,n,i,j;
+	cin>>t;
+	while (t--)
+	{
+		
+	}
+	return 0;
+}
+
+/*
+	cin>>n;
+		int a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+
+	string s;
+		cin>>s;
+
+	ifstream cin("input.txt");
+	ofstream cout("output.txt");
+
+	#pragma GCC optimize "trapv"
+*/
